be4f61ecdbac5e45524d39b126c83801925f1c64
Jose <pastorpedraza3@gmail.com>
2021-06-14 14:24:13 -0500

    Fix routes for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :books
-  resources :users
+  resources :users, only: [:show, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
